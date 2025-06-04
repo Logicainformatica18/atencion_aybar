a349e88d4276079e70799f8f76f27ca10ed64e58
Anthony <logicainformatica18@gmail.com>
2025-06-04 16:27:22 -0500

    update ssl

--- resources/js/lib/echo.ts
+++ resources/js/lib/echo.ts
@@ -3,20 +3,26 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
-const host = import.meta.env.VITE_REVERB_HOST || 'localhost';
-const port = import.meta.env.VITE_REVERB_PORT || 8080;
-const scheme = import.meta.env.VITE_REVERB_SCHEME || 'http';
+const host = import.meta.env.VITE_REVERB_HOST;
+const port = Number(import.meta.env.VITE_REVERB_PORT);
+const scheme = import.meta.env.VITE_REVERB_SCHEME;
+
+console.log('🌐 Echo config:', { host, port, scheme });
 
 const echoInstance = new Echo({
   broadcaster: 'pusher',
   key: import.meta.env.VITE_REVERB_APP_KEY,
+  cluster: 'mt1',
   wsHost: host,
   wsPort: port,
   wssPort: port,
-  forceTLS: scheme === 'https',
-  enabledTransports: ['ws', 'wss'],
-  cluster: 'mt1', // 👈 Agrega esta línea para evitar el error
+  forceTLS: true,
+  encrypted: true,
+  disableStats: true,
+  enabledTransports: ['wss'],
 });
+
+
 window.Echo = echoInstance;
 
 export default echoInstance;
